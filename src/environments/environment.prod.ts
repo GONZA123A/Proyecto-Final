c98fcb4e79ea9cbe32dd1ee6fc15c544e64f62e0
ampi03 <amparola03p@gmail.com>
2022-11-22 11:05:07 -0300

    todo jaja

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyARKTddhAyDnjMEWifvw7ruXqxt_QeOvHg",
+    authDomain: "proyecto-final-decb8.firebaseapp.com",
+    projectId: "proyecto-final-decb8",
+    storageBucket: "proyecto-final-decb8.appspot.com",
+    messagingSenderId: "648848737978",
+    appId: "1:648848737978:web:0d6fd700c7662038a16483"
+  }
 };
